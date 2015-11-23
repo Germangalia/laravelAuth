ede661f8ce19fb34c125cc2df1e75b78dca2ea93
Germangalia <germangalia@iesebre.com>
2015-11-23 17:27:04 +0100

    Afegida funció de email amb Mailgun en laravel

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -32,9 +32,39 @@ class RegisterController extends Controller
 
         $user->save();
 
+        $this->email = $request->get('email');
+        $this->name = $request->get('name');
+
+        $this->sendRegisterEmail();
+
         //User::create(Input::all());
         //User::create(request->all());
 
         return redirect()->route('auth.login');
+    }
+
+    public function sendRegisterEmail(){
+
+        $emailData = new \stdClass();
+
+        $emailData->email = $this->email;
+        $emailData->name = $this->name;
+        $emailData->subject = "welcome user" . $this->name;
+        //$emailData->pathToFile = '/home/ggalia84/Imágenes/authentication_1.png';
+
+        //$emailData->footer = "welcome user" . $this->footer;
+        //$emailData->header = "welcome user" . $this->header;
+
+        //$data = "HOLA";
+        $data['name'] = $this->name;
+        $data['var2'] = "Valor2";
+
+        \Mail::send('emails.message', $data, function($message) use ($emailData){
+            $message->from(env('CONTACT_MAIL'), env('CONTACT_NAME'));
+            $message->to($emailData->email, $emailData->name);
+            $message->subject($emailData->subject);
+            //$message->attach($emailData->pathToFile);
+        });
+
     }
 }
